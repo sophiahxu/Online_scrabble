bc183349df587334b8251d5c776d9d9118c359b0
Hanna Westerheim <hkw27@cornell.edu>
2021-10-05 21:25:37 -0400

    update filesystem, make play working

--- _build/default/bin/main.ml
+++ _build/default/bin/main.ml
@@ -1,5 +1,15 @@
 open Graphics
 
-let main () = open_graph ""
+exception Exit
+
+let main () =
+  open_graph "";
+  try
+    while true do
+      let st = wait_next_event [ Key_pressed ] in
+      if st.keypressed then raise Exit
+    done
+  with
+  | Exit -> ()
 
 let () = main ()
