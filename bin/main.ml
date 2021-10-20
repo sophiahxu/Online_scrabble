7bd0c4c8ca19c0b0fef5ef56b7b81a8a10ae170c
Hanna Westerheim <hkw27@cornell.edu>
2021-10-19 22:10:56 -0400

    revisions for ideomaticness

--- bin/main.ml
+++ bin/main.ml
@@ -2,29 +2,29 @@ open Graphics
 
 exception Exit
 
-let main () =
+let init () =
   open_graph "";
-  try
-    moveto 150 200;
-    draw_string "Let's Play Scrabble! Click the Play Button to Start";
-    set_text_size 35;
-    set_color yellow;
 
-    set_window_title "SCRABBLE";
-    let vertices = [| (250, 250); (250, 350); (350, 300) |] in
-    draw_poly vertices;
-    fill_poly vertices;
-    let cursor = wait_next_event [ Button_down ] in
+  moveto 150 200;
+  draw_string "Let's Play Scrabble! Click the Play Button to Start";
+  set_text_size 35;
+  set_color yellow;
+
+  set_window_title "SCRABBLE";
+  let vertices = [| (250, 250); (250, 350); (350, 300) |] in
+  draw_poly vertices;
+  fill_poly vertices
+
+let event_loop st =
+  if st.keypressed then raise Exit;
+  if st.button then
     if
-      cursor.mouse_x > 250 && cursor.mouse_x < 350
-      && cursor.mouse_y > 250 && cursor.mouse_y < 350
-    then if cursor.button then open_graph "";
+      st.mouse_x > 250 && st.mouse_x < 350 && st.mouse_y > 250
+      && st.mouse_y < 350
+    then open_graph ""
 
-    while true do
-      let st = wait_next_event [ Key_pressed ] in
-      if st.keypressed then raise Exit
-    done
-  with
-  | Exit -> ()
+let main () =
+  init ();
+  loop_at_exit [ Key_pressed; Button_down ] event_loop
 
 let () = main ()
