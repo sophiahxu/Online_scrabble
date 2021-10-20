294ecd4d3a1fc11dc7a5a8726e65bca5f2c59095
Sophia Xu <shx3@cornell.edu>
2021-10-20 16:32:38 -0400

    upding sophia branch

--- Makefile
+++ Makefile
@@ -9,3 +9,19 @@ test:
 
 play:
 	OCAMLRUNPARAM=b dune exec bin/main.exe
+
+zip:
+	rm -f scrabble.zip
+	zip -r scrabble.zip . -x@exclude.lst
+
+doc:
+	dune build @doc
+
+clean:
+	dune clean
+	rm -f scrabble.zip
+
+loc:
+	dune clean
+	rm -f scrabble.zip
+	cloc --by-file --include-lang=OCaml .
--- src/author.mli
+++ src/author.mli
@@ -1,6 +1,5 @@
 (** CS 3110 Fall 2021 Assignment Final Project
-
     @author Sophia Xu (shx3), Hanna Westerheim (hkw27), Grace (gjl56) *)
 
- val hours_worked : int
- (** [hours_worked] is the number of hours you worked on this assignment. *)
+    val hours_worked : int
+    (** [hours_worked] is the total person hours worked by the team. *)
