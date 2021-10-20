74523675fc000c45a59732cd515d45ce2f27399e
Hanna Westerheim <hkw27@cornell.edu>
2021-10-19 23:46:53 -0400

    add documentation

--- bin/main.ml
+++ bin/main.ml
@@ -2,7 +2,10 @@ open Graphics
 open Game
 
 exception Exit
+(** Raised when the GUI should be closed. *)
 
+(** [init ()] initializes the game with a welcoming message and button
+    allows the player to play the game. *)
 let init () =
   open_graph "";
 
@@ -16,6 +19,8 @@ let init () =
   draw_poly vertices;
   fill_poly vertices
 
+(** [event_loop st] takes the player to the next screen if the button is
+    pressed. Raises: [Exit] if a key is pressed. *)
 let event_loop st =
   if st.keypressed then raise Exit;
   if st.button then
@@ -24,6 +29,9 @@ let event_loop st =
       && st.mouse_y < 350
     then Game_board.make_board ()
 
+(** [main ()] initializes and maintains the home screen and switches to
+    the play screen when the button is pressed. If a key is pressed, the
+    GUI closes. *)
 let main () =
   init ();
   loop_at_exit [ Key_pressed; Button_down ] event_loop
