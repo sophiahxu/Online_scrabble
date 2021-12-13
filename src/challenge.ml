41d8bf531be9a5e0517163bcbac35445d9a9d6f2
Sophia Xu <shx3@cornell.edu>
2021-12-13 15:22:32 -0500

    challenge somewhat working?

--- src/challenge.ml
+++ src/challenge.ml
@@ -14,37 +14,6 @@ type t = {
   finished : bool;
 }
 
-let init () =
-  { mode = Challenger_query; loser = None; finished = false }
-(*TODO: change as needed.*)
-
-let draw c =
-  (*TODO*)
-  match c.mode with
-  | Challenger_query -> ()
-  | Dictionary_lookup -> ()
-  | Loser_query -> ()
-  | Continue_query -> ()
-
-let click x y c =
-  (*TODO*)
-  match c.mode with
-  | Challenger_query -> c
-  | Dictionary_lookup -> c
-  | Loser_query -> c
-  | Continue_query -> c
-
-let loser c = c.loser
-
-let finished c = c.finished
-
-(*----------------------------------------------------------------------------*)
-
-(*just for testing purposes, delete later*)
-let testing =
-  open_graph "";
-  resize_window 900 625
-
 (*[draw_rects num] draws a row of rectangles, with its associated number
   inside it, with lower left corner at [y]*)
 let rec draw_rects num y =
@@ -57,7 +26,7 @@ let rec draw_rects num y =
       draw_string (string_of_int num);
       draw_rects (num - 1) y
 
-let start =
+let query () =
   set_color white;
   fill_rect 30 30 140 565;
   set_color blue;
@@ -78,12 +47,16 @@ let which_player x =
   else if x > 130 && x < 160 then 4
   else 0
 
-let look_up =
+let return_int () =
   let s = wait_next_event [ Button_down ] in
   let x = s.mouse_x in
-  let num = which_player x in
+  which_player x
+
+let look_up () =
+  (* let s = wait_next_event [ Button_down ] in let x = s.mouse_x in let
+     num = which_player x in *)
   moveto 40 475;
-  draw_string ("Player " ^ string_of_int num ^ ", look up in ");
+  draw_string "Players, look up in ";
   moveto 40 460;
   draw_string "a dictionary whether ";
   moveto 40 445;
@@ -96,25 +69,57 @@ let look_up =
   moveto 85 385;
   draw_string "DONE"
 
-let question =
-  let s = wait_next_event [ Button_down ] in
-  if
-    s.mouse_x > 70 && s.mouse_x < 120 && s.mouse_y > 375
-    && s.mouse_y < 415
-  then moveto 40 350;
+let loser () =
+  (* let s = wait_next_event [ Button_down ] in if s.mouse_x > 70 &&
+     s.mouse_x < 120 && s.mouse_y > 375 && s.mouse_y < 415 then*)
+  moveto 40 350;
   draw_string "Which player lost the";
   moveto 40 335;
   draw_string "challenge?";
   draw_rects 4 295
 
-let winner =
-  let s = wait_next_event [ Button_down ] in
-  let x = s.mouse_x in
-  let num = which_player x in
+let continue () =
+  (* let s = wait_next_event [ Button_down ] in let x = s.mouse_x in let
+     num = which_player x in *)
   moveto 40 270;
-  draw_string ("Player " ^ string_of_int num ^ ", your next");
+  draw_string "The losing player's";
   moveto 40 255;
   draw_string "turn will be skipped.";
-  num
+  moveto 40 240;
+  draw_string "Click return to go";
+  moveto 40 225;
+  draw_string "back to the game.";
+  draw_rect 70 180 50 30;
+  moveto 80 190;
+  draw_string "Return"
 
-(*let init = start; look_up; question; winner*)
+let init () =
+  { mode = Challenger_query; loser = None; finished = false }
+(*TODO: change as needed.*)
+
+let draw c =
+  match c.mode with
+  | Challenger_query -> query ()
+  | Dictionary_lookup -> look_up ()
+  | Loser_query -> loser ()
+  | Continue_query -> continue ()
+
+let click x y c =
+  match c.mode with
+  | Challenger_query ->
+      if y > 500 && y < 530 && x > 40 && x < 160 then
+        { mode = Dictionary_lookup; loser = None; finished = false }
+      else c
+  | Dictionary_lookup ->
+      if x > 70 && x < 120 && y > 375 && y < 415 then
+        { mode = Loser_query; loser = None; finished = false }
+      else c
+  | Loser_query ->
+      if y > 295 && y < 325 then
+        let num = Some (which_player x) in
+        { mode = Continue_query; loser = num; finished = false }
+      else c
+  | Continue_query -> { c with finished = true }
+
+let loser c = c.loser
+let finished c = c.finished
--- src/challenge.mli
+++ src/challenge.mli
@@ -5,8 +5,8 @@ val init : unit -> t
 (**[init ()] initializes the challenge gameplay mode.*)
 
 val click : int -> int -> t -> t
-(**[click x y c] returns un updated challenge [c] according to the [x]
-   and [y] components of the click input.*)
+(**[click x y c] returns updated challenge [c] according to the [x] and
+   [y] components of the click input.*)
 
 val draw : t -> unit
 (**[draw c] draws the challenge [c] to the screen.*)
@@ -17,8 +17,3 @@ val loser : t -> int option
 
 val finished : t -> bool
 (**[finished c] is whether the challenge mode is finished.*)
-
-(*val init : int (**[init] starts the challenge mode by drawing a
-  challenge box in place of the key, and asks what player is
-  challenging. [init] returns the integer of the player that lost the
-  challenge*)*)
